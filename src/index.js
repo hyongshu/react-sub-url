7060ed2d984703d36af47b4e4ddd70dcc4f2b578
Anusit Chamornmano <anusit@Anusits-MacBook-Pro.local>
2020-05-11 19:53:31 +0700

    netlify

--- src/index.js
+++ src/index.js
@@ -1,46 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import {
-  Route,
-  NavLink,
-  BrowserRouter as Router,
-  Switch,
-} from "react-router-dom";
-import App from "./App";
-import Users from "./users";
-import Contact from "./contact";
-import Notfound from "./notfound";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "./styles.css";
 
-const routing = (
-  <Router basename={process.env.PUBLIC_URL}>
-    <div>
-      <ul>
-        <li>
-          <NavLink exact activeClassName="active" to="/">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName="active" to="/users">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName="active" to="/contact">
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-      <hr />
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/users" component={Users} />
-        <Route path="/contact" component={Contact} />
-        <Route component={Notfound} />
-      </Switch>
-    </div>
-  </Router>
-);
+const Main = () => <h1>Main!</h1>;
+const Elsewhere = () => <h1>Elsewhere!</h1>;
 
-ReactDOM.render(routing, document.getElementById("root"));
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Main</Link>
+            </li>
+            <li>
+              <Link to="/else">Else</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/else/" component={Elsewhere} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
